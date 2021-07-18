d4d3d51ddf956ac941c94353f72b9670e515910c
Marvin Wu <marvin.wu@urbanspring.hk>
2021-07-18 10:45:06 +0800

    cli working

--- test/preTest.sh
+++ test/preTest.sh
@@ -1,2 +1,7 @@
 rm -f tmp/test.db
 sqlite3 tmp/test.db < test/input/init.sql
+node bin/hitl-helper create tmp/test.db 'select * from a' id test
+node bin/hitl-helper  update tmp/test.db 1
+node bin/hitl-helper assign tmp/test.db 1
+node bin/hitl-helper get tmp/test.db 1 | jq
+node bin/hitl-helper submit tmp/test.json  tmp/test.db 1 
